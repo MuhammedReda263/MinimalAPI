b90b808b15c80bef5a93660105370f2951338316
Muhammed Reda <moreda263@gmail.com>
2025-09-21 05:04:37 +0300

    GET and POST with Minimal API

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,22 @@
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+List<Product> productsList = new List<Product>() { new Product { Id = 1, ProductName = "Ipad" }, new Product {Id=2, ProductName = "Iphone"} };
+
+app.MapGet("/product", async (HttpContext context) => {
+    var products = string.Join("\n" ,productsList.Select(temp=>temp.ToString()));
+    await context.Response.WriteAsync(products);
+   }
+);
+
+app.MapPost("/product", async (HttpContext context,Product product) => {
+    productsList.Add(product);
+    await context.Response.WriteAsync("Product added successfully");
+   }
+);
+
 
 app.Run();
